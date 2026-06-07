8bfbe2fef2392455504ba9f424e9b0781170fbe6
Dev Kumar <devkmandal0@gmail.com>
2026-06-07 14:26:07 +0530

    Add Express and WebSocket servers, introduce Prisma for database management

--- apps/http-backend/index.ts
+++ apps/http-backend/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from 'express';
+
+
+const app = express();
+
+
+
+
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    console.log("Server is running on ",3000);
+})
--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import {WebSocket, WebSocketServer} from 'ws';
+
+
+const wss = new WebSocketServer({
+    port:8080
+})
+
+wss.on('connection',async(ws ,req)=>{
+    console.log('Websocket is running on 8080')
+  
+})
